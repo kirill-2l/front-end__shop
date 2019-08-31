0be6d5275afb7cf1557a777d560ff25a418674fd
Kirill Petrov <kirill@Kirills-MacBook-Air.local>
2019-08-31 21:51:55 +0300

    add block information and about(tabs)

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -50,16 +50,18 @@ $(document).ready(function () {
 
 		let tabs = function () {
 			$('.tabs-navigation__item').click(function() {
-				let tabName = $(this).attr('show-tab');
+				let tabName = $(this).attr('show-tab'),
+						tabsBody = $(this).closest('.tabs').find('.tabs__body')[0],
+						tab = $(tabsBody).find('.' + tabName);
 				$(this).addClass('tabs-navigation__item--active').siblings().removeClass('tabs-navigation__item--active');
-				$('.tabs__body .' + tabName).addClass('tab--active').siblings().removeClass('tab--active');
-				if ('.tabs__body .' + tabName + ' .js-products-line-slider') {
+				$(tab).addClass('tab--active').siblings().removeClass('tab--active');
+				if ($(tabsBody).find('.js-products-line-slider').length) {
 					$('.js-products-line-slider').each(function () {
 						$(this).slick('refresh');
 					});
 					$('.js-product-prev__slider').each(function() {
 						$(this).slick('refresh');
-					})
+					});
 				}
 			});
 
